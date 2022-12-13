7d3d35d2c05974241628b42f22d5151fb6b6a943
shvmpz05 <martinkimani914@gmail.com>
2022-12-13 14:04:58 +0300

    More on chapter 2

--- c4e_chapter2/length.c
+++ c4e_chapter2/length.c
@@ -6,12 +6,15 @@
  * Return: 0 Always Success
 */
 
+
 int mystrlen(char s[]);
+unsigned int htoi(char s[]);
+
 
 int main(void)
 {
     
-    printf("The lenght of this string is %d\n", mystrlen("Hello World"));
+    printf("The lenght of this string is %d\n", mystrlen("Hello World"));s
 }
 
 int mystrlen(char s[])
@@ -24,4 +27,44 @@ int mystrlen(char s[])
         ++i;
     }
     return i;
+}
+
+
+/**
+ * htoi - changes string of hexadecimal numbers to their int counterparts
+ * @s: string to check for 
+ * 
+ * Return: integer or the hexadecimal
+*/
+
+int htoi(char s[])
+{
+    int hexdigit, i, inhex, n;
+    i = 0;
+
+    if(s[i] == '0')
+    {
+        ++i;
+        if(s[i] == 'x' || s[i] == 'X')
+            ++i;
+    }
+
+    n = 0;
+    inhex = YES;
+
+    for(;inhex == YES; ++i)
+        hexdigit = s[i] - '0';
+    else if(s[i] >= 'a' && s[i] <= 'f')
+        hexdigit = s[i] - 'a' + 10;
+    else if(s[i] >= 'A' && s[i] <= 'F')
+        hexdigit = s[i] - 'A' + 10;
+    else
+        inhex = NO;
+
+    if(inhex == YES)
+    {
+        n = 16 * n + hexdigit;
+    }
+
+    return n;   
 }
